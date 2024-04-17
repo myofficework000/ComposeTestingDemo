21770ed7fc4d5db2699c25a67f1813cb0c7768d8
TerryTaoYY <yueyang.tao@gmail.com>
2024-04-17 11:00:04 -0400

    create login and login test case.

--- app/src/androidTest/java/abhishek/pathak/composetesting/LoginUITest.kt
+++ app/src/androidTest/java/abhishek/pathak/composetesting/LoginUITest.kt
@@ -1,17 +1,9 @@
 package abhishek.pathak.composetesting
 
 import abhishek.pathak.composetesting.ui.theme.ComposeTestingTheme
-import androidx.compose.ui.test.SemanticsNodeInteraction
-import androidx.compose.ui.test.assert
-import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.performTextClearance
-import androidx.compose.ui.test.performTextInput
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Assert.assertNotEquals
-import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -23,11 +15,6 @@ class LoginUITest {
     @get:Rule
     val rule = createComposeRule()
 
-    private lateinit var email: SemanticsNodeInteraction
-    private lateinit var password: SemanticsNodeInteraction
-    private lateinit var loginButton: SemanticsNodeInteraction
-    private lateinit var loginLabel: SemanticsNodeInteraction
-
     @Before
     fun setUp() {
         rule.setContent {
@@ -35,73 +22,29 @@ class LoginUITest {
                 LoginScreen {}
             }
         }
-
-        with(rule) {
-            email = onNodeWithTag("email")
-            password = onNodeWithTag("password")
-            loginButton = onNodeWithTag("LoginButton")
-            loginLabel = onNodeWithTag("LoginLabel")
-        }
     }
 
     @Test
     fun verifyAllViewsAreExists() {
-        with(rule) {
-            email.assertExists()
-            password.assertExists()
-            loginLabel.assertExists()
-            loginButton.assertExists()
-        }
+        rule.onNodeWithTag("LoginLabel").assertExists()
+        rule.onNodeWithTag("email").assertExists()
+        rule.onNodeWithTag("password").assertExists()
+        rule.onNodeWithTag("LoginButton").assertExists()
     }
 
     @Test
     fun verifyEmailAddressValidation() {
-        with(email) {
-            performTextInput(VALID_EMAIL)
-            assert(hasText(VALID_EMAIL))
-            assertTrue(!isEmailValid.value)
-            assertTrue(emailErrMsg.value == "")
-            performTextClearance()
-        }
-    }
-
-    @Test
-    fun verifyEmailAddressValidationWithInvalidInput() {
-
-        val invalidEmail = "myofficework"
-
-        with(email) {
-            performTextInput(invalidEmail)
-            assert(hasText(invalidEmail))
-            assertNotEquals(VALID_EMAIL, hasText(invalidEmail))
-            performTextClearance()
-        }
-    }
-
-
-    @Test
-    fun verifyConfirmPasswordValidationWithValidInput() {
-
-        val input = "123456"
-
-        with(password) {
-            performTextInput(input)
-            assert(hasText(input))
-            assertTrue(!isConfirmPasswordInvalid.value)
-            assertTrue(emailErrMsg.value == "")
-            performTextClearance()
+        val validEmail = "test@example.com"
+        rule.onNodeWithTag("email").apply {
+            performTextInput(validEmail)
+            assert(hasText(validEmail))
         }
     }
 
     @Test
     fun loginUser() {
-        email.performTextInput("myemail@example.com")
-        password.performTextInput("12345678")
-        loginButton.performClick()
-    }
-
-    private companion object {
-
-        const val VALID_EMAIL = "myofficework@gmail.com"
+        rule.onNodeWithTag("email").performTextInput("myemail@example.com")
+        rule.onNodeWithTag("password").performTextInput("12345678")
+        rule.onNodeWithTag("LoginButton").performClick()
     }
 }
--- app/src/main/java/abhishek/pathak/composetesting/Login.kt
+++ app/src/main/java/abhishek/pathak/composetesting/Login.kt
@@ -1,14 +1,10 @@
 package abhishek.pathak.composetesting
-import android.content.Context
-import android.util.Log
-import android.widget.Toast
+
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -22,7 +18,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,12 +26,11 @@ import androidx.compose.ui.unit.sp
 @Preview
 @Composable
 private fun LoginScreenUIPrev() {
-
     LoginScreen{}
 }
 
 @Composable
-fun LoginScreen(    onLogin: () -> Unit) {
+fun LoginScreen(onLogin: () -> Unit) {
     var email by remember { mutableStateOf(TextFieldValue()) }
     var password by remember { mutableStateOf(TextFieldValue()) }
 
@@ -61,18 +55,16 @@ fun LoginScreen(    onLogin: () -> Unit) {
         PasswordTextField(password) { password = it }
         SpacerSmallLogin()
 
-        SimpleTextButton("Login") {
-        }
+        SimpleTextButton("Login", onLogin)
     }
 }
-
 
 @Composable
 fun EmailTextField(email: TextFieldValue, onEmailChange: (TextFieldValue) -> Unit) {
     OutlinedTextField(
         value = email,
         leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "emailIcon") },
-        onValueChange = { onEmailChange(it) },
+        onValueChange = onEmailChange,
         label = { Text(text = "Email") },
         modifier = Modifier.testTag("email")
     )
@@ -83,7 +75,7 @@ fun PasswordTextField(password: TextFieldValue, onPasswordChange: (TextFieldValu
     OutlinedTextField(
         value = password,
         leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "lockIcon") },
-        onValueChange = { onPasswordChange(it) },
+        onValueChange = onPasswordChange,
         label = { Text(text = "Password") },
         modifier = Modifier.testTag("password")
     )
@@ -92,7 +84,7 @@ fun PasswordTextField(password: TextFieldValue, onPasswordChange: (TextFieldValu
 @Composable
 fun SimpleTextButton(buttonMessage: String, onClick: () -> Unit) {
     Button(
-        onClick = { onClick() },
+        onClick = onClick,
         modifier = Modifier.testTag("LoginButton"),
         colors = ButtonDefaults.buttonColors(Color.Blue)
     ) {
@@ -103,17 +95,4 @@ fun SimpleTextButton(buttonMessage: String, onClick: () -> Unit) {
 @Composable
 fun SpacerSmallLogin() {
     Spacer(modifier = Modifier.padding(4.dp))
-}
-
-fun showToastLogin(context: Context, string: String) {
-    Toast.makeText(context, string, Toast.LENGTH_SHORT).show()
-}
-
-@Composable
-fun showToastMessageLogin(context: Context, message: String) {
-    showToastLogin(context, message)
-}
-
-fun showLongToastLogin(context: Context, string: String) {
-    Toast.makeText(context, string, Toast.LENGTH_LONG).show()
 }
